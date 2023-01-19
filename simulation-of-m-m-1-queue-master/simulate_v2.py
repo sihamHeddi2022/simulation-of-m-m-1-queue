d496c9937f5183d37887ba5322d91861796d3245
siham heddi <siham.heddi.dev@gmail.com>
2023-01-19 22:37:37 +0100

    cc

---
+++ simulation-of-m-m-1-queue-master/simulate_v2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random
+import math
+
+
+def expo(p):
+    return (-1/p)*math.log(random.random())
+
+
+def simulate(lamda, mu, tempsMax):
+
+    #examples = []
+    inter_arrival = expo(lamda)
+    ct = inter_arrival
+    t_arrival = ct
+    depart_t = 0
+    waiting = []
+    nb = {}
+    while ct < tempsMax:
+
+        nb[inter_arrival] = inter_arrival/tempsMax
+        #print(ct)
+
+        service_time = expo(mu)
+        nb[service_time] = service_time/tempsMax
+
+        depart_t = ct +service_time
+        #examples.append((t_arrival,depart_t))
+        waiting.append(depart_t-t_arrival)
+
+        inter_arrival =  expo(lamda)
+        t_arrival = ct + inter_arrival
+        if t_arrival < depart_t:
+            ct = depart_t
+        else:
+            ct = t_arrival
+    avg_customers = sum(s*nb[s] for s in nb)
+    avg_wait = sum(waiting)/len(waiting)
+    return avg_wait,avg_customers
+
+
+
+print(simulate(6,5,10))
+
+
+
+
+
